f9efe501ad9a66760b6361ae8aa33422b9438249
hugosenari <hugosenari@gmail.com>
2022-03-12 04:15:00 -0300

    chore: re index crowncalc

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Basic calculator in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."crowncalc-master".url = "path:./master";
+  inputs."crowncalc-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."crowncalc-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = ''Basic calculator in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-crowncalc-master.flake = false;
+  inputs.src-crowncalc-master.owner = "RainbowAsteroids";
+  inputs.src-crowncalc-master.ref   = "refs/heads/master";
+  inputs.src-crowncalc-master.repo  = "crowncalc";
+  inputs.src-crowncalc-master.type  = "github";
+  
+  inputs."noise".dir   = "nimpkgs/n/noise";
+  inputs."noise".owner = "riinr";
+  inputs."noise".ref   = "flake-pinning";
+  inputs."noise".repo  = "flake-nimble";
+  inputs."noise".type  = "github";
+  inputs."noise".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."noise".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."sdl2".dir   = "nimpkgs/s/sdl2";
+  inputs."sdl2".owner = "riinr";
+  inputs."sdl2".ref   = "flake-pinning";
+  inputs."sdl2".repo  = "flake-nimble";
+  inputs."sdl2".type  = "github";
+  inputs."sdl2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."sdl2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-crowncalc-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-crowncalc-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
