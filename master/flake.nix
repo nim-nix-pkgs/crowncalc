90efa114962594bfd7f3143857c63b8e774dfd33
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:54 -0300

    chore: re-index ../nimpkgs/c/crowncalc/master

--- master/flake.nix
+++ master/flake.nix
@@ -9,13 +9,14 @@
   
   inputs.src-crowncalc-master.flake = false;
   inputs.src-crowncalc-master.owner = "RainbowAsteroids";
-  inputs.src-crowncalc-master.ref   = "refs/heads/master";
+  inputs.src-crowncalc-master.ref   = "master";
   inputs.src-crowncalc-master.repo  = "crowncalc";
   inputs.src-crowncalc-master.type  = "github";
   
   inputs."noise".owner = "nim-nix-pkgs";
   inputs."noise".ref   = "master";
   inputs."noise".repo  = "noise";
+  inputs."noise".dir   = "v0_1_14";
   inputs."noise".type  = "github";
   inputs."noise".inputs.nixpkgs.follows = "nixpkgs";
   inputs."noise".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
@@ -23,6 +24,7 @@
   inputs."sdl2".owner = "nim-nix-pkgs";
   inputs."sdl2".ref   = "master";
   inputs."sdl2".repo  = "sdl2";
+  inputs."sdl2".dir   = "v2_0_3";
   inputs."sdl2".type  = "github";
   inputs."sdl2".inputs.nixpkgs.follows = "nixpkgs";
   inputs."sdl2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
